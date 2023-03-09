0a7fc38aedbb117a66246e0b37825079ca3592ed
Yelleti Krishna <krishna.yelleti@publicissapient.com>
2023-03-09 16:02:53 +0530

    Just checking another day

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -4,18 +4,21 @@ scanner(
     serverUrl: "http://localhost:9000",
     login: "admin",
     password: "Hello@123",
+    token: "29fa8b64df23dc5b26074133de259fbc02c2502d",
     options: {
+      "sonar.projectName": "sonarqube-react-project",
+      "sonar.projectKey": "sonarqube-react-project",
+      "sonar.projectVersion": "0.0.1",
       "sonar.sources": "./src",
-      "sonar.exclusions": "**/*.test.jsx",
+      // "sonar.exclusions": "**/*.test.jsx",
       "sonar.tests": "./src",
       "sonar.test.inclusions": "**/*.test.jsx,**/*.test.js",
-      "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-      "sonar.testExecutionReportPaths": "test-report.xml",
+      "sonar.javascript.lcov.reportPaths": "./coverage/lcov.info",
+      "sonar.testExecutionReportPaths": "./test-report.xml",
     },
   },
   () => process.exit()
 );
-// const scanner = require('sonarqube-scanner');
 
 // scanner(
 //     {
